915db777be5e9c5196b2f309a3061040f560b733
Bilal Mustafa <bmustafase@gmail.com>
2024-03-08 15:31:24 +0500

    Version 1.0

--- test/index.js
+++ test/index.js
@@ -1,19 +1,21 @@
-const util = require('util');
+const util = require("util");
 const scraper = require("../dist").GoogleFormsScraper();
 
-const input = { url: "https://docs.google.com/forms/d/e/1FAIpQLSdMQCw_BzKaN8DjRVh-Xxn11zDT424SHX_HTmoFyMJQZepTJA/viewform" }
-    //Act
-    scraper.getFormTemplate(input)
-        //Assert
-        .then(response => {
-            console.log(util.inspect(response, { showHidden: false, depth: null, colors: true }));
-        }).then(() => {
-           console.log("Hello")
-        })
-
-
-
+const input = {
+  url:
+    "https://docs.google.com/forms/d/e/1FAIpQLSdMQCw_BzKaN8DjRVh-Xxn11zDT424SHX_HTmoFyMJQZepTJA/viewform"
+};
+//Act
+scraper
+  .getFormTemplate(input)
+  //Assert
+  .then(response => {
+    console.log(
+      util.inspect(response, { showHidden: false, depth: null, colors: true })
+    );
+  })
+  .then(() => {
+    console.log("Hello");
+  });
 
 //Full Form: https://docs.google.com/forms/d/e/1FAIpQLSeG7cGqUZ4FhohjWkv3Ei_AYWEiAR9A4qk-ZV1qOUWUs8FNLw/viewform
-
-
